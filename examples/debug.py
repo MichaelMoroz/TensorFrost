ebe8726bc5bacbf3d79f48d71b8ae96e4ccc7dd1
Mykhailo Moroz <michael08840884@gmail.com>
2023-12-05 01:07:05 +0200

    Update examples to use proper arguments

--- examples/debug.py
+++ examples/debug.py
@@ -3,8 +3,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import time
 
-tf.initialize(tf.cpu, "H:/cl_compile.bat /Zi")
-#tf.initialize(tf.cpu, "H:/cl_compile.bat /O2 /fp:fast /openmp:experimental /Zi")
+tf.initialize(tf.cpu, "/Zi")
+#tf.initialize(tf.cpu, "/O2 /fp:fast /openmp:experimental /Zi")
 
 n0 = 32
 m0 = 64
@@ -64,8 +64,8 @@ def ComputeColor():
 
     return [vx * 255.0]
 
-N = 1024 
-M = 320
+N = 512 
+M = 512
 
 def Bilinear(tex, x, y):
     xi, yi = tf.floor(x), tf.floor(y)
@@ -117,47 +117,103 @@ def RK4Advection(vx, vy, dt):
     x4, y4 = x - (vx + 2.0*vx1 + 2.0*vx2 + vx3)*dt/6.0, y - (vy + 2.0*vy1 + 2.0*vy2 + vy3)*dt/6.0
     return x4, y4
 
-def SemiLagrange(vx, vy, pressure, dt):
-    # advect velocity
-    #x1, y1 = RK4Advection(vx, vy, dt)
-    x1, y1 = EulerAdvection(vx, vy, dt)
+def SemiLagrange(vx, vy, pressure, density, dt):
+    x1, y1 = RK4Advection(vx, vy, dt)
+    #x1, y1 = EulerAdvection(vx, vy, dt)
 
     #vx = CubicInterp(vx, x1, y1)
     #vy = CubicInterp(vy, x1, y1)
     #pressure = CubicInterp(pressure, x1, y1)
-    vx = CubicInterp(vx, x1, y1)
-    vy = CubicInterp(vy, x1, y1)
+    vx = Bilinear(vx, x1, y1)
+    vy = Bilinear(vy, x1, y1)
     #pressure = Bilinear(pressure, x1, y1)
+    #densitylin = Bilinear(density, x1, y1)
+    #dens1 = densitylin*0.99
+    #dens2 = densitylin*1.00
+    #dens3 = tf.min(dens1, dens2)
+    #dens4 = tf.max(dens1, dens2)
+    density = Bilinear(density, x1, y1)
 
-    return [vx, vy, pressure]
+    return [vx, vy, pressure, density]
 
-def BFECC(vx, vy, pressure, dt):
+def BFECC(vx, vy, pressure, density, dt):
     i, j = vx.indices
     x, y = tf.float(i), tf.float(j)
     
     # advect backwards
     x1, y1 = x - vx*dt, y - vy*dt
     vx1, vy1 = Bilinear(vx, x1, y1), Bilinear(vy, x1, y1)
+    density1 = Bilinear(density, x1, y1)
 
     # advect forwards
     x2, y2 = x + vx*dt, y + vy*dt
     vx2, vy2 = Bilinear(vx1, x2, y2), Bilinear(vy1, x2, y2)
+    density2 = Bilinear(density1, x2, y2)
 
     # compute backwards forwards error correction
     vx = vx + (vx - vx2)*0.5
     vy = vy + (vy - vy2)*0.5
+    density = density + (density - density2)*0.5
 
     # advect corrected backwards
     vx3, vy3 = Bilinear(vx, x1, y1), Bilinear(vy, x1, y1)
+    density3 = Bilinear(density, x1, y1)
 
-    return [vx3, vy3, pressure]
+    return [vx3, vy3, pressure, density3]
 
-def Jacobi(pressure, div):
+def Boundary(i, j):
+    N1, M1 = i.shape
+    return 1.0 - tf.float((i < 3) | (i > N1-4) | (j < 3) | (j > M1-4))
+
+def Jacobi(pressure, div, iterations):
     i, j = pressure.indices
 
-    # pressure solve
-    pressure = (pressure[i-1, j] + pressure[i+1, j] + pressure[i, j-1] + pressure[i, j+1] - div) / 4.0
+    edge = Boundary(i, j)
 
+    # pressure solve
+    for it in range(iterations):
+        pressure = edge * (pressure[i-1, j] + pressure[i+1, j] + pressure[i, j-1] + pressure[i, j+1] - div) / 4.0
+
+    return pressure
+
+def Restrict(field):
+    N1, M1 = field.shape
+    N2, M2 = N1/2, M1/2
+    i, j = tf.indices([N2, M2])
+    i, j = 2*i, 2*j
+    return 0.25*(field[i, j] + field[i+1, j] + field[i, j+1] + field[i+1, j+1])
+
+def Prolong(field, orig):
+    i, j = orig.indices
+    i, j = i/2, j/2
+    return orig + field[i, j]
+
+def Residual(pressure, div):
+    i, j = pressure.indices
+    return div - (pressure[i-1, j] + pressure[i+1, j] + pressure[i, j-1] + pressure[i, j+1] - 4.0*pressure)
+
+def VCycle(pressure, div):
+    pressure = Jacobi(pressure, div, 1)
+
+    res = Residual(pressure, div)
+    res = Restrict(res)
+    pressure0 = Jacobi(tf.zeros(res.shape), 4.0*res, 8)
+
+    #Currently not working
+    #res1 = Residual(pressure0, 4.0*res)
+    #res1 = Restrict(res1)
+    #pressure1 = Jacobi(tf.zeros(res1.shape), 16.0*res1, 8)
+    #pressure0 = Prolong(pressure1, pressure0)
+
+    pressure = Prolong(pressure0, pressure)
+
+    pressure = Jacobi(pressure, div, 1)
+
+    return pressure
+
+def PressureSolve(pressure, div):
+    pressure = VCycle(pressure, div)
+    pressure = VCycle(pressure, div)
     return pressure
 
 def Smoothstep(edge0, edge1, x):
@@ -169,42 +225,74 @@ def FluidTest():
     vx = tf.input([N, M], tf.float32)
     vy = tf.input([N, M], tf.float32)
     pressure = tf.input([N, M], tf.float32)
+    density = tf.input([N, M], tf.float32)
+    canvas = tf.zeros([N, M, 3], tf.float32)
 
     dt = 1.0
     i,j = vx.indices
     x, y = tf.float(i), tf.float(j)
 
-    vx, vy, pressure = SemiLagrange(vx, vy, pressure, dt)
+    vx, vy, pressure, density = SemiLagrange(vx, vy, pressure, density, dt)
     
     # add source
-    source = 0.26*tf.exp(-((x-N/5.0)**2.0 + (y-2.0*M/3.0)**2.0)/100.0)
-    source = source - 0.25*tf.exp(-((x-4.0*N/5.0)**2.0 + (y-M/3.0)**2.0)/100.0)
-    vx = vx + source
+    source = 0.16*tf.exp(-((y-M/5.0)**2.0 + (x-2.0*N/3.0)**2.0)/100.0)
+    source = source - 0.15*tf.exp(-((y-4.0*M/5.0)**2.0 + (x-N/3.0)**2.0)/100.0)
+    vy = vy + source
+    density = density + source*source
 
-    edge = 1.0 - tf.float((i < 2) | (i > N-3) | (j < 2) | (j > M-3))
+    edge = Boundary(i, j)
     vx = vx * edge
     vy = vy * edge
+    density = tf.clamp(density * edge, -0.5, 1.0)
 
     # pressure solve
     # compute divergence
     div = (vx[i+1, j] - vx[i-1, j] + vy[i, j+1] - vy[i, j-1]) / 2.0
+    curl = tf.abs(vy[i+1, j] - vy[i-1, j] - vx[i, j+1] + vx[i, j-1]) / 2.0
 
-    # pressure solve
-    for it in range(2):
-        pressure = pressure * edge
-        pressure = Jacobi(pressure, div)
-    
+    pressure = PressureSolve(pressure, div)
+
     # subtract pressure gradient
     gradx = (pressure[i+1, j] - pressure[i-1, j])*1.0
     grady = (pressure[i, j+1] - pressure[i, j-1])*1.0
     vx = vx - gradx
     vy = vy - grady
 
-    mag = 0.2*tf.sqrt(vx*vx + vy*vy)
+    # vortex confinement
 
-    r, g, b = 255.0*Smoothstep(0.0, 0.33, mag), 255.0*Smoothstep(0.33, 0.66, mag), 255.0*Smoothstep(0.66, 1.0, mag)
+    # compute gradient of curl magnitude
+    gradx = (curl[i+1, j] - curl[i-1, j])*1.0
+    grady = (curl[i, j+1] - curl[i, j-1])*1.0
 
-    return [vx, vy, pressure, r, g, b]
+    # normalize gradient
+    mag = tf.sqrt(gradx*gradx + grady*grady) + 1e-5
+    gradx = gradx / mag
+    grady = grady / mag
+
+    # compute vortex force
+    vortx = -grady * curl
+    vorty = gradx * curl
+
+    # add vortex force
+    vort_scale = 0.2
+    vx = vx + vortx * dt * vort_scale
+    vy = vy + vorty * dt * vort_scale
+
+    #mag = 0.2*tf.sqrt(vx*vx + vy*vy)
+    mag = 2.5*density
+
+    #canvas[i, j, 0] = 255.0*Smoothstep(0.0, 0.33, mag)
+    #canvas[i, j, 1] = 255.0*Smoothstep(0.33, 0.66, mag)
+    #canvas[i, j, 2] = 255.0*Smoothstep(0.66, 1.0, mag)
+    canvas[i, j, 0] = 255.0 * (0.277 + mag * (0.105 + mag * (-0.330 + mag * (-4.634 + mag * (6.228 + mag * (4.776 - 5.435 * mag))))))
+    canvas[i, j, 1] = 255.0 * (0.005 + mag * (1.404 + mag * (0.214 + mag * (-5.799 + mag * (14.179 + mag * (-13.745 + 4.645 * mag))))))
+    canvas[i, j, 2] = 255.0 * (0.334 + mag * (1.384 + mag * (0.095 + mag * (-19.332 + mag * (56.690 + mag * (-65.353 + 26.312 * mag))))))
+
+    return [vx, vy, pressure, canvas, div, density, Residual(pressure, div)]
+
+
+tf.initialize(tf.cpu, "")
+fluid = tf.program(FluidTest)
 
 def WaveIteration(u, v, dt):
     i,j = u.indices
