710a4fd242f07b1b2ae37608642c3d77d9866dae
Dmytro Bulatov <dmytro.bulatov@boolka.dev>
2023-12-04 22:17:56 +0200

    another build fix

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='TensorFrost',
     version='0.1',
-    packages=[''],
-    package_dir={'': 'python_module'},
-    package_data={'': ['*.pyd']},
+    packages=['TensorFrost'],
+    package_dir={'TensorFrost': 'python_module'},
+    package_data={'TensorFrost': ['*.pyd']},
 )
