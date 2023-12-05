e2b6d0541ae4f3da289e2154618e06504ef629ac
Mykhailo Moroz <michael08840884@gmail.com>
2023-12-05 16:16:08 +0200

    Python version rebuild script

--- clean_rebuild.cmd
+++ clean_rebuild.cmd
@@ -1,4 +1,5 @@
-git clean -d -f -x
-cmake -DPYTHON_VERSION=3.12 -S . -B build 
+git clean -d -x -f -e PythonBuild/dist
+cmake -DPYBIND11_PYTHON_VERSION=3.12 -S . -B build 
 cmake --build build
+py -3.12 -m pip install build
 py -3.12 -m build ./PythonBuild
