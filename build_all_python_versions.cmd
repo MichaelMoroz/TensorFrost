c8de4b932a2045cbc32f289fb66712123828dbbf
Mykhailo Moroz <michael08840884@gmail.com>
2023-12-05 18:07:15 +0200

    Update build_all_python_versions.cmd

--- build_all_python_versions.cmd
+++ build_all_python_versions.cmd
@@ -2,9 +2,8 @@
 setlocal
 
 REM Define the list of Python versions
-set PYTHON_VERSIONS=3.8 3.9 3.10 3.11 3.12
+set PYTHON_VERSIONS=3.7 3.8 3.9 3.10 3.11 3.12
 
-REM Loop over each version and call your existing script
 for %%v in (%PYTHON_VERSIONS%) do (
     echo Building for Python %%v
     call clean_rebuild %%v
