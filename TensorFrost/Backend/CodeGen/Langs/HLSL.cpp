b9e7bfda1f00c2652bb640e542f3c715af4f9558
Mykhailo Moroz <michael08840884@gmail.com>
2024-03-17 04:58:09 +0200

    Update HLSL.cpp

--- TensorFrost/Backend/CodeGen/Langs/HLSL.cpp
+++ TensorFrost/Backend/CodeGen/Langs/HLSL.cpp
@@ -7,253 +7,38 @@ using namespace std;
 
 string GenerateHLSLKernel(Program* program, const Kernel* kernel) {
 	string final_source = R"(
-#include <cmath>
-#include <omp.h>
-#include <initializer_list>
-#include <functional>
-#include <vector>
-#include <atomic>
-
-typedef unsigned int uint;
-
-inline int min(int a, int b)
-{
-  return a < b ? a : b;
-}
-
-inline int max(int a, int b)
-{
-  return a > b ? a : b;
-}
-
-inline float min(float a, float b)
-{
-  return a < b ? a : b;
-}
-
-inline float max(float a, float b)
-{
-  return a > b ? a : b;
-}
-
-inline float asfloat(uint x)
-{
-  return *(float*)&x;
-}
-
-inline uint asuint(float x)
-{
-  return *(uint*)&x;
-}
-
-inline uint asuint(int x)
-{
-  return *(uint*)&x;
-}
-
-inline uint asuint(uint x)
-{
-  return *(uint*)&x;
-}
-
-inline int asint(uint x)
-{
-  return *(int*)&x;
-}
-
-inline int clamp(int x, int a, int b)
-{
-  return min(max(x, a), b);
-}
-
-inline float clamp(float x, float a, float b)
-{
-  return min(max(x, a), b);
-}
-
-inline float lerp(float a, float b, float t)
-{
-  return a + (b - a) * t;
-}
-
-inline void InterlockedAdd(int* memory, int address, int value)
-{
-  std::atomic<int>* place = reinterpret_cast<std::atomic<int>*>(&memory[address]);
-  place->fetch_add(value, std::memory_order_relaxed);
-}
-
-inline void InterlockedAdd(uint* memory, int address, uint value)
-{
-  std::atomic<uint>* place = reinterpret_cast<std::atomic<uint>*>(&memory[address]);
-  place->fetch_add(value, std::memory_order_relaxed);
-}
-
-inline void InterlockedAdd(float* memory, int address, float value)
-{
-  std::atomic<float>* place = reinterpret_cast<std::atomic<float>*>(&memory[address]);
-  float current = place->load(std::memory_order_relaxed);
-  float goal = current + value;
-  while (!place->compare_exchange_weak(current, goal, std::memory_order_release, std::memory_order_relaxed)) {
-      goal = current + value;
-  }
-}
-
-inline int InterlockedAdd_Prev(int* memory, int address, int value)
-{
-  std::atomic<int>* place = reinterpret_cast<std::atomic<int>*>(&memory[address]);
-  return place->fetch_add(value, std::memory_order_relaxed);
-}
-
-inline uint InterlockedAdd_Prev(uint* memory, int address, uint value)
-{
-  std::atomic<uint>* place = reinterpret_cast<std::atomic<uint>*>(&memory[address]);
-  return place->fetch_add(value, std::memory_order_relaxed);
-}
-
-inline float InterlockedAdd_Prev(float* memory, int address, float value)
-{
-  std::atomic<float>* place = reinterpret_cast<std::atomic<float>*>(&memory[address]);
-  float current = place->load(std::memory_order_relaxed);
-  float goal = current + value;
-  while (!place->compare_exchange_weak(current, goal, std::memory_order_release, std::memory_order_relaxed)) {
-      goal = current + value;
-  }
-  return current;
-}
-
-inline void InterlockedAnd(int* memory, int address, int value)
-{
-  std::atomic<int>* place = reinterpret_cast<std::atomic<int>*>(&memory[address]);
-  place->fetch_or(value, std::memory_order_relaxed);
-}
-
-inline void InterlockedAnd(uint* memory, int address, uint value)
-{
-  std::atomic<uint>* place = reinterpret_cast<std::atomic<uint>*>(&memory[address]);
-  place->fetch_and(value, std::memory_order_relaxed);
-}
-
-inline void InterlockedOr(int* memory, int address, int value)
-{
-  std::atomic<int>* place = reinterpret_cast<std::atomic<int>*>(&memory[address]);
-  place->fetch_or(value, std::memory_order_relaxed);
-}
-
-inline void InterlockedOr(uint* memory, int address, uint value)
-{
-  std::atomic<uint>* place = reinterpret_cast<std::atomic<uint>*>(&memory[address]);
-  place->fetch_or(value, std::memory_order_relaxed);
-}
-
-inline void InterlockedXor(int* memory, int address, int value)
-{
-  std::atomic<int>* place = reinterpret_cast<std::atomic<int>*>(&memory[address]);
-  place->fetch_xor(value, std::memory_order_relaxed);
-}
-
-inline void InterlockedXor(uint* memory, int address, uint value)
-{
-  std::atomic<uint>* place = reinterpret_cast<std::atomic<uint>*>(&memory[address]);
-  place->fetch_xor(value, std::memory_order_relaxed);
-}
-
-inline void InterlockedMin(int* memory, int address, int value)
-{
-  std::atomic<int>* place = reinterpret_cast<std::atomic<int>*>(&memory[address]);
-  int current = place->load(std::memory_order_relaxed);
-  int goal = min(current, value);
-  while (!place->compare_exchange_weak(current, goal, std::memory_order_release, std::memory_order_relaxed)) {
-      goal = min(current, value);
-  }
-}
-
-inline void InterlockedMin(float* memory, int address, float value)
-{
-  std::atomic<float>* place = reinterpret_cast<std::atomic<float>*>(&memory[address]);
-  float current = place->load(std::memory_order_relaxed);
-  float goal = min(current, value);
-  while (!place->compare_exchange_weak(current, goal, std::memory_order_release, std::memory_order_relaxed)) {
-      goal = min(current, value);
-  }
-}
-
-inline void InterlockedMax(int* memory, int address, int value)
-{
-  std::atomic<int>* place = reinterpret_cast<std::atomic<int>*>(&memory[address]);
-  int current = place->load(std::memory_order_relaxed);
-  int goal = max(current, value);
-  while (!place->compare_exchange_weak(current, goal, std::memory_order_release, std::memory_order_relaxed)) {
-      goal = max(current, value);
-  }
-}
-
-inline void InterlockedMax(float* memory, int address, float value)
-{
-  std::atomic<float>* place = reinterpret_cast<std::atomic<float>*>(&memory[address]);
-  float current = place->load(std::memory_order_relaxed);
-  float goal = max(current, value);
-  while (!place->compare_exchange_weak(current, goal, std::memory_order_release, std::memory_order_relaxed)) {
-      goal = max(current, value);
-  }
-}
-
-inline uint pcg(uint v)
+uint pcg(uint v)
 {
 	uint state = v * 747796405u + 2891336453u;
 	uint word = ((state >> ((state >> 28u) + 4u)) ^ state) * 277803737u;
 	return (word >> 22u) ^ word;
 }
 
-inline float pcgf(uint v)
+float pcgf(uint v)
 {
-	return (float)pcg(v) / (float)0xffffffffu;
+	return float(pcg(v)) / float(0xffffffffu)
 }
 
-void dispatch(void(*kernel)(uint*, uint*, uint*, uint*), uint* mem, std::initializer_list<uint> off, std::initializer_list<uint> var, std::initializer_list<uint> shape)
+RWStructuredBuffer<uint> mem : register(u0);
+
+struct UBO
 {
-  uint* off_arr = new uint[off.size()];
-  uint* var_arr = new uint[var.size()];
-  uint* shape_arr = new uint[shape.size()];
+	uint var[32];
+};
 
-  for (int i = 0; i < off.size(); i++)
-  {
-    off_arr[i] = off.begin()[i];
-  }
+cbuffer ubo : register(b1) { UBO ubo; }
 
-  for (int i = 0; i < var.size(); i++)
-  {
-	var_arr[i] = var.begin()[i];
-  }
-
-  for (int i = 0; i < shape.size(); i++)
-  {
-	shape_arr[i] = shape.begin()[i];
-  }
-
-  kernel(var_arr, off_arr, mem, shape_arr);
-
-  delete[] off_arr;
-  delete[] var_arr;
-  delete[] shape_arr;
-} 
-
-uint allocate(uint alloc(uint*&, uint*, uint dim), uint*& mem, std::initializer_list<uint> shape)
+[numthreads(256, 1, 1)]
+void main(uint3 dtid : SV_DispatchThreadID, uint3 lid : SV_GroupThreadID)
 {
-  uint* shape_arr = new uint[shape.size()];
-
-  for (int i = 0; i < shape.size(); i++)
-  {
-	shape_arr[i] = shape.begin()[i];
-  }
-
-  uint off = alloc(mem, shape_arr, shape.size());
-
-  delete[] shape_arr;
-
-  return off;
-}
-
+	int thread_id = dtid.x;
 )";
+
+	CodeGenerator generator;
+	generator.GenerateKernelCode(kernel);
+	string kernel_code = generator.AssembleString();
+
+	final_source += AddIndent(kernel_code, "    ") + "\n}\n";
 
     return final_source;
 	
