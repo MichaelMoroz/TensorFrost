7b3812234b13a316e19d308110e7fc30c5bd599e
Mykhailo Moroz <michael08840884@gmail.com>
2024-07-20 17:28:22 +0300

    Increment version

--- PythonBuild/setup.py
+++ PythonBuild/setup.py
@@ -6,7 +6,7 @@ with open("../README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='TensorFrost',
-    version='0.5.8',
+    version='0.5.9',
     author="Mykhailo Moroz",
     author_email="michael08840884@gmail.com",
     description="Tensor library with automatic kernel fusion",
