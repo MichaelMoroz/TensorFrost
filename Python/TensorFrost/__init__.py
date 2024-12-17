9d0c000cde06becfedec1977e470b26bc1cef960
Mykhailo Moroz <michael08840884@gmail.com>
2024-12-17 02:05:58 +0100

    Add sorting module

--- Python/TensorFrost/__init__.py
+++ Python/TensorFrost/__init__.py
@@ -4,4 +4,5 @@ from . import optimizers
 from . import regularizers
 from . import clipping
 from . import random
+from . import sort
 from .default import *
--- tests/sorting_test.py
+++ tests/sorting_test.py
@@ -102,13 +102,19 @@ def BitonicSort(keys, values):
 def Sort0():
     keys = tf.input([-1], tf.int32)
     values = tf.input([-1], tf.int32)
-    sorted_keys, sorted_values = RadixSort(keys, values)
+    sorted_keys, sorted_values = tf.sort.radix(keys, values)
     return sorted_keys, sorted_values
 
 def Sort1():
     keys = tf.input([-1], tf.int32)
     values = tf.input([-1], tf.int32)
-    sorted_keys, sorted_values = BitonicSort(keys, values)
+    sorted_keys, sorted_values = tf.sort.bitonic(keys, values)
+    return sorted_keys, sorted_values
+
+def Sort2():
+    keys = tf.input([-1], tf.float32)
+    values = tf.input([-1], tf.float32)
+    sorted_keys, sorted_values = tf.sort.radix(keys, values)
     return sorted_keys, sorted_values
 
 class TestSorting(unittest.TestCase):
@@ -116,8 +122,9 @@ class TestSorting(unittest.TestCase):
         #compile the program
         sort_program0 = tf.compile(Sort0)
         sort_program1 = tf.compile(Sort1)
+        sort_program2 = tf.compile(Sort2)
 
-        # Generate some random data to scan (ints between 0 and 10)
+        # Generate some random data to sort (ints between 0 and 10)
         N = 2**20
         MaxValue = 2**31 - 1
         keys = np.random.randint(0, MaxValue, N).astype(np.int32)
@@ -137,3 +144,20 @@ class TestSorting(unittest.TestCase):
         error_bitonic = np.sum(np.abs(sorted_keys1.numpy - sorted_keys2))
         self.assertTrue(error_radix == 0)
         self.assertTrue(error_bitonic == 0)
+
+        # Generate some random data to sort (floats between 0 and 1)
+        keys = np.random.rand(N).astype(np.float32)
+        values = np.random.rand(N).astype(np.float32)
+
+        # do sort in TensorFrost
+        sorted_keys0, sorted_values0 = sort_program2(keys, values)
+
+        # do argsort in numpy
+        sorted_indices = np.argsort(keys)
+        sorted_keys2 = keys[sorted_indices]
+        sorted_values2 = values[sorted_indices]
+
+        # check if the results are the same
+        error_radix = np.sum(np.abs(sorted_keys0.numpy - sorted_keys2))
+        self.assertTrue(error_radix == 0)
+        
