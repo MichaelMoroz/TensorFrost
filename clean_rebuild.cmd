8b72237b00970bd008696ce807bb2cacac318d70
Mykhailo Moroz <47035925+MichaelMoroz@users.noreply.github.com>
2026-03-25 18:00:26 +0100

    Add Python 3.14

--- clean_rebuild.cmd
+++ clean_rebuild.cmd
@@ -1,6 +1,6 @@
 @echo off
 set PYTHON_VERSION=%1
-if "%PYTHON_VERSION%"=="" set PYTHON_VERSION=3.12
+if "%PYTHON_VERSION%"=="" set PYTHON_VERSION=3.14
 
 git clean -d -x -f -e Python/dist
 py -%PYTHON_VERSION% -m pip install -e Python/ -v
